3c40c25a9fee5fab7e5661b221bfa45f1ae1792a
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-04-09 21:52:58 +0530

    Update product.tf

--- product.tf
+++ product.tf
@@ -1,1 +1,16 @@
+resource "azurerm_api_management_product" "product" {
+  product_id          = "order-product"
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
 
+  display_name          = "Order Product"
+  subscription_required = true
+  published             = true
+}
+
+resource "azurerm_api_management_product_api" "link_api" {
+  api_name            = azurerm_api_management_api.order_api.name
+  product_id          = azurerm_api_management_product.product.product_id
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
+}

321ee55e3185a9f94219c4392a779b04b251ccae
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-04-09 21:50:39 +0530

    Update apim.tf

--- apim.tf
+++ apim.tf
@@ -1,1 +1,8 @@
-
+resource "azurerm_api_management" "apim" {
+  name                = var.apim_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  publisher_name      = var.publisher_name
+  publisher_email     = var.publisher_email
+  sku_name            = "Developer_1"
+}

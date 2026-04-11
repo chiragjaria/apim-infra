c351f5154de9a3edf566e88a92ff6206177afecf
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-04-11 17:52:13 +0530

    Update api.tf

--- api.tf
+++ api.tf
@@ -1,6 +1,6 @@
 resource "azurerm_api_management_api" "order_api" {
   name                = "order-api"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   api_management_name = azurerm_api_management.apim.name
   revision            = "1"
   display_name        = "Order API"
@@ -14,7 +14,7 @@ resource "azurerm_api_management_api_operation" "create_order" {
   operation_id        = "create-order"
   api_name            = azurerm_api_management_api.order_api.name
   api_management_name = azurerm_api_management.apim.name
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   display_name = "Create Order"
   method       = "POST"
@@ -24,7 +24,7 @@ resource "azurerm_api_management_api_operation" "create_order" {
 resource "azurerm_api_management_api_policy" "policy" {
   api_name            = azurerm_api_management_api.order_api.name
   api_management_name = azurerm_api_management.apim.name
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   xml_content = file("${path.module}/policy.xml")
 }

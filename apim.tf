2468bc0851011ce56e7b9e154aa76004c7cb3e23
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-04-11 17:51:44 +0530

    Update apim.tf

--- apim.tf
+++ apim.tf
@@ -1,7 +1,7 @@
 resource "azurerm_api_management" "apim" {
   name                = var.apim_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   publisher_name      = var.publisher_name
   publisher_email     = var.publisher_email
   sku_name            = "Developer_1"
